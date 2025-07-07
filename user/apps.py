a18ba6a4385d4e8bd69657f216a395908cceb683
Kintu Peter <kintupeter721@gmail.com>
2025-07-08 02:41:23 +0300

    Implement automatic video transcoding using FFmpeg in post_save signal

--- user/apps.py
+++ user/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user'
 
-    class UserConfig(AppConfig):
-        default_auto_field = 'django.db.models.BigAutoField'
-        name = 'user'
+    def ready(self):
+        import user.signals # noqa: F401
 
-        def ready(self):
-            import user.signals # noqa: F401
-    
+
